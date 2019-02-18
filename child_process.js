28f13e57a4e7780186b75d5fbcd2cb49c4e8564f
Chirag Pandit <chirag.pandit@here.com>
2019-02-18 11:32:21 +0530

    Added tests for Pizza CLI Program

--- child_process.js
+++ child_process.js
@@ -1,16 +1,97 @@
 
-
-
 //import run,{ ENTER } from 'inquirer-test';
+
+//Assertion
+const assert = require("assert");
+
 const run = require("inquirer-test");
 const ENTER = require("inquirer-test").ENTER;
 const UP = require("inquirer-test").UP;
 const DOWN = require("inquirer-test").DOWN;
 
+//Child Process Exec Command
 var exec = require("child_process").exec;
 
+//Pizza CLI Program
 const cliPath = "pizza";
 
+describe("Verify Pizza Command", function () {
+
+
+    it("Should show help on providing -h param", function (done) {
+        exec("pizza -h", function (error, output, stderr) {
+            if (error || stderr) {
+                throw new Error("pizza -h failed \n Error : " + error + "\n stdError: " + stderr);
+            }
+            //console.log("output :" +output);
+
+            done();
+        });
+    });
+
+
+    it("Should order with marble cheese on -m param", function (done) {
+        exec("pizza -n", function (error, output, stderr) {
+            if (error || stderr) {
+                throw new Error("pizza -m failed \n Error : " + error + "\n stdError: " + stderr);
+            }
+            
+            let strExpectedOutput = "you ordered a pizza with:\n"
+            " - marble cheese"; 
+            //output = "Hello";
+            if (output.indexOf(strExpectedOutput) != -1){
+                //Pass
+                done();
+            }
+            else {
+                throw new Error("Unable to order pizza with pizza -n param");
+            }
+           //assert.notEqual(output.indexOf(strExpectedOutput), 1);
+           //done();
+        });
+    });
+
+    it("Should prompt user for entering user name", async function () {
+        
+        const result = await run([cliPath], [""]);
+        //console.log("result: " + result);
+        
+        var strExpectedOutput = "Welcome to pizza shop, Please enter the pizza type..!"+
+        "\n? Tell us your first name:";
+
+        if (result.indexOf(strExpectedOutput) != -1){
+            //Pass
+        }
+        else {
+            throw new Error("No prompt present asking username when user goes into CLI");
+        }
+        
+    });
+
+
+    it("Prompt user when invalid phone number '>> Please enter a valid phone number - 123-456-7777'", async function () {
+        
+        const result = await run([cliPath], [
+            ENTER,
+            ENTER,
+            "1234567" + ENTER]);
+        //console.log("result: " + result);
+        
+        var strExpectedOutput = ">> Please enter a valid phone number - 123-456-7777";
+        if (result.indexOf(strExpectedOutput) != -1){
+            //Pass
+        }
+        else {
+            throw new Error("Validation message not present when user types in invalid phone number");
+        }
+    });
+
+    //Validate successful 
+
+});
+
+
+/*
 async function test() {
     const result = await run([cliPath], [
         ENTER,
@@ -44,9 +125,8 @@ async function test() {
 
 };
 test();
-
-
-
+*/
+/*
 // var assert = require('assert');
 // const spawn = require('child_process').spawn;
 // const concat = require('concat-stream');
@@ -98,5 +178,4 @@ test();
 //         );
 //     });
 //     return promise;
-// }
-// module.exports = { execute };
+// 
